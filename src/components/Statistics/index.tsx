f9d47cb86b2580d8a5232f0a22badc8b4d9c85c8
fiimaarif <fii.ma'arif@baf.id>
2023-09-26 19:33:17 +0700

    [REFACTOR] Statistics Item

--- src/components/Statistics/index.tsx
+++ src/components/Statistics/index.tsx
@@ -1,40 +1,22 @@
 export function Statistics() {
+  const statisticItems = [
+    {number: "1", name: "Creative Space"},
+    {number: "3", name: " Community Platforms"},
+    {number: "12", name: "Memorandum of Understandings"},
+    {number: "18", name: "Sustainable Events"},
+    {number: "75+", name: "Bekasi Communities"},
+    {number: "10000+", name: "Participants"},
+  ]
   return (
     <section className="my-20 px-5 lg:px-0 font-poppins">
       <h1 className="text-center text-5xl mb-10">Statistik</h1>
       <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
-        <div className="flex flex-col items-center justify-center">
-          <dt className="mb-2 text-3xl font-extrabold">1</dt>
-          <dd className="text-gray-500 text-center">Creative Space</dd>
+        {statisticItems.map((item, index) => (
+        <div key={index} className="flex flex-col items-center justify-center">
+          <dt className="mb-2 text-3xl font-extrabold">{item.number}</dt>
+          <dd className="text-gray-500 text-center">{item.name}</dd>
         </div>
-        <div className="flex flex-col items-center justify-center">
-          <dt className="mb-2 text-3xl font-extrabold">3</dt>
-          <dd className="text-gray-500 text-center">
-            Community Platforms
-          </dd>
-        </div>
-        <div className="flex flex-col items-center justify-center">
-          <dt className="mb-2 text-3xl font-extrabold">12</dt>
-          <dd className="text-gray-500 text-center">
-            Memorandum of Understandings
-          </dd>
-        </div>
-        <div className="flex flex-col items-center justify-center">
-          <dt className="mb-2 text-3xl font-extrabold">18</dt>
-          <dd className="text-gray-500 text-center">
-            Sustainable Events
-          </dd>
-        </div>
-        <div className="flex flex-col items-center justify-center">
-          <dt className="mb-2 text-3xl font-extrabold">75+</dt>
-          <dd className="text-gray-500 text-center">
-            Bekasi Communities
-          </dd>
-        </div>
-        <div className="flex flex-col items-center justify-center">
-          <dt className="mb-2 text-3xl font-extrabold">10000+</dt>
-          <dd className="text-gray-500 text-center">Participants</dd>
-        </div>
+        ))}
       </div>
     </section>
   );
