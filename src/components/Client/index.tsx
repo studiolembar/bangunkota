a79912d5330b934f7463a0422fc73093063bfb8a
fiimaarif <fii.ma'arif@baf.id>
2023-10-14 15:29:11 +0700

    [UPDATE] Collaborator image

--- src/components/Client/index.tsx
+++ src/components/Client/index.tsx
@@ -3,24 +3,24 @@ import Carousel from 'react-multi-carousel';
 
 export function Client(){
     const clientItems = [
-      { url: "/assets/images/clients/governments/Government1.png" },
-      { url: "/assets/images/clients/governments/Government2.png" },
-      { url: "/assets/images/clients/governments/Government3.png" },
-      { url: "/assets/images/clients/governments/Government4.png" },
-      { url: "/assets/images/clients/business/Business1.png" },
-      { url: "/assets/images/clients/business/Business2.png" },
-      { url: "/assets/images/clients/business/Business3.png" },
-      { url: "/assets/images/clients/business/Business4.png" },
-      { url: "/assets/images/clients/business/Business5.png" },
-      { url: "/assets/images/clients/business/Business6.png" },
-      { url: "/assets/images/clients/business/Business7.png" },
-      { url: "/assets/images/clients/business/Business8.png" },
-      { url: "/assets/images/clients/academics/Academic1.png" },
-      { url: "/assets/images/clients/academics/Academic2.png" },
-      { url: "/assets/images/clients/academics/Academic3.png" },
-      { url: "/assets/images/clients/media/Media1.png" },
-      { url: "/assets/images/clients/media/Media2.png" },
-      { url: "/assets/images/clients/media/Media3.png" },
+      { url: "/assets/images/clients/kolaborator-1.png" },
+      { url: "/assets/images/clients/kolaborator-2.png" },
+      { url: "/assets/images/clients/kolaborator-3.png" },
+      { url: "/assets/images/clients/kolaborator-4.png" },
+      { url: "/assets/images/clients/kolaborator-5.png" },
+      { url: "/assets/images/clients/kolaborator-6.png" },
+      { url: "/assets/images/clients/kolaborator-7.png" },
+      { url: "/assets/images/clients/kolaborator-8.png" },
+      { url: "/assets/images/clients/kolaborator-9.png" },
+      { url: "/assets/images/clients/kolaborator-10.png" },
+      { url: "/assets/images/clients/kolaborator-11.png" },
+      { url: "/assets/images/clients/kolaborator-12.png" },
+      { url: "/assets/images/clients/kolaborator-13.png" },
+      { url: "/assets/images/clients/kolaborator-14.png" },
+      { url: "/assets/images/clients/kolaborator-15.png" },
+      { url: "/assets/images/clients/kolaborator-16.png" },
+      { url: "/assets/images/clients/kolaborator-17.png" },
+      { url: "/assets/images/clients/kolaborator-18.png" },
     ];
     const responsive = {
       superLargeDesktop: {
@@ -75,7 +75,6 @@ export function Client(){
                 <div key={index}>
                   <Image
                     src={item.url}
-                    className="w-20 h-16 lg:w-full lg:h-full"
                     width={150}
                     height={0}
                     alt="client"
